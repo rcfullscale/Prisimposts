be1e46e62d90b29ef38a2acc6ce1c69e50cac6c5
rcfulscale <114207070+rcfulscale@users.noreply.github.com>
2022-10-29 08:52:17 +1000

    Add files via upload

---
+++ Src/Static/login.js
@@ -0,0 +1,8 @@
+console.log("Hello,world");
+function login (p1, p2) {
+  const input = prompt("What's your name?");
+  if (input == "$#1login") {
+    alert("Login sucess");
+    window.open('E:/PRISM%20posts/Src/dev/index.html');
+  }
+}
